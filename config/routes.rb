c60b7b9564d5a875387fe72ccc3b8f60b566169a
Isabel <isanpe@gmail.com>
2022-08-27 14:58:01 -0400

    Onceavo commit agrega controlador home y vistas Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'home/index'
   devise_for :admins
 
-  authenticate :admin do
-
-  resources :products
-  resources :categories
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root 'homes#index'
+   authenticate :admin do
+    resources :products
+    resources :categories
+  end
   
+  root 'home#index'
 end
